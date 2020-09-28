e697714fc2f7319011f468494655f0e188801d68
ge-cyber-developer <61585687+ge-cyber-developer@users.noreply.github.com>
2020-09-28 16:13:51 -0300

    swagger e criptografia

--- Repositories/UsuarioRepository.cs
+++ Repositories/UsuarioRepository.cs
@@ -4,12 +4,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace API_Edux.Repositories
 {
     public class UsuarioRepository : IUsuario
     {
-        
+        static string sha256(string randomString)
+{
+        var crypt = new SHA256Managed();
+        string hash = String.Empty;
+        byte[] crypto = crypt.ComputeHash(Encoding.ASCII.GetBytes(randomString));
+        foreach (byte theByte in crypto)
+        {
+       
+        hash += theByte.ToString("x2");
+        }
+    return hash;
+}
 
         public Usuario Alterar(int id, Usuario j)
         {
