c5b92b427928b2d0fc00f8e421b9b906d992c574
unknown <villagepllayer@gmail.comgit co>
2020-09-28 19:52:13 -0300

    Classe Curtida Finalizada e funcionando

---
+++ Controllers/CurtidaController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API_Edux.Domains;
+using API_Edux.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API_Edux.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CurtidaController : ControllerBase
+    {
+        CurtidaRepository repo = new CurtidaRepository();
+        // GET: api/<CurtidaController>
+        [HttpGet]
+        public   List<Curtida> Get()
+        {
+            return repo.LerTodos();
+        }
+
+        // GET api/<CurtidaController>/5
+        [HttpGet("{id}")]
+        public Curtida Get(int id)
+        {
+            return repo.BuscarPorId(id);
+        }
+
+        // POST api/<CurtidaController>
+        [HttpPost]
+        public Curtida Post([FromBody] Curtida l)
+        {
+            return repo.Cadastrar(l);
+        }
+
+        // PUT api/<CurtidaController>/5
+        [HttpPut("{id}")]
+        public Curtida Put(int id, [FromBody] Curtida l)
+        {
+            return repo.Alterar(id, l);
+        }
+
+        // DELETE api/<CurtidaController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            repo.Excluir(id);
+        }
+    }
+}
--- Repositories/CurtidaRepository.cs
+++ Repositories/CurtidaRepository.cs
@@ -1,5 +1,7 @@
-﻿using API_Edux.Domains;
+﻿using API_Edux.Contexts;
+using API_Edux.Domains;
 using API_Edux.Interfaces;
+using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,31 +11,96 @@ namespace API_Edux.Repositories
 {
     public class CurtidaRepository : ICurtida
     {
-        
+
+        //Ligando Com o Context
+        EduxContext conexao = new EduxContext();
+
+        SqlCommand cmd = new SqlCommand();
+
 
         public Curtida Alterar(int id, Curtida l)
         {
-            throw new NotImplementedException();
+            cmd.Connection = conexao.Conectar();
+
+            cmd.CommandText = "UPDATE Curtida SET IdDica = @IdDica WHERE IdCurtida = @id";
+
+            cmd.Parameters.AddWithValue("@Id", id);
+
+            cmd.ExecuteNonQuery();
+            //encerrando Conexão
+            conexao.Desconectar();
+            return l;
         }
 
         public Curtida BuscarPorId(int id)
         {
-            throw new NotImplementedException();
+            cmd.Connection = conexao.Conectar();
+
+            cmd.CommandText = "SELECT * FROM Curtida WHERE IdCurtida = @id";
+
+            cmd.Parameters.AddWithValue("@id", id);
+
+            SqlDataReader dados = cmd.ExecuteReader();
+
+            Curtida i = new Curtida();
+            while (dados.Read())
+            {
+                i.IdCurtida = Convert.ToInt32(dados.GetValue(0));
+                i.IdDica = Convert.ToInt32(dados.GetValue(1));
+                i.IdUsuario = Convert.ToInt32(dados.GetValue(2));
+            }
+            conexao.Desconectar();
+            return i;
         }
 
         public Curtida Cadastrar(Curtida l)
         {
-            throw new NotImplementedException();
+            cmd.Connection = conexao.Conectar();
+
+            cmd.CommandText =
+                "INSERT INTO Curtida(IdDica)" +
+                "VALUES" +
+                "(@IdDica)";
+            cmd.Parameters.AddWithValue("@IdDica", l.IdDica);
+
+            cmd.ExecuteNonQuery();
+            return l;
         }
 
         public void Excluir(int id)
         {
-            throw new NotImplementedException();
+            cmd.Connection = conexao.Conectar();
+
+            cmd.CommandText = "DELETE FROM Curtida WHERE IdCurtida = @id";
+            cmd.Parameters.AddWithValue("@id", id);
+
+            cmd.ExecuteNonQuery();
+            conexao.Desconectar();
         }
 
         public List<Curtida> LerTodos()
         {
-            throw new NotImplementedException();
+            cmd.Connection = conexao.Conectar();
+            cmd.CommandText = "SELECT FROM Curtida";
+
+            SqlDataReader dados = cmd.ExecuteReader();
+            List<Curtida> curtidas = new List<Curtida>();
+            while (dados.Read())
+            {
+                curtidas.Add(
+                    new Curtida()
+                    {
+                        IdCurtida = Convert.ToInt32(dados.GetValue(0)),
+                        IdDica = Convert.ToInt32(dados.GetValue(1)),
+                        IdUsuario = Convert.ToInt32(dados.GetValue(2))
+
+
+                    }
+
+                    );
+            }
+            conexao.Desconectar();
+            return curtidas;
         }
     }
 }
